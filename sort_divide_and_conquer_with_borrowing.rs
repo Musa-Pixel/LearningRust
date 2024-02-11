82c68506f34bdb76eac028aa05a058e5eff44757
mpixel <68288167+Musa-Pixel@users.noreply.github.com>
2024-02-11 23:13:39 +0600

    Update sort_divide_and_conquer_with_borrowing.rs

--- sort_divide_and_conquer_with_borrowing.rs
+++ sort_divide_and_conquer_with_borrowing.rs
@@ -1,32 +1,34 @@
 fn main() {
     let mut arr = Vec::from([23, 23, 1, 90, 12]);
-    sort_dac(&mut arr);
+    arr = sort_dac(arr);
 
     for item in arr {
         println!("{}", item);
     }
 }
 
-fn sort_dac(arr: &mut Vec<i32>) {
+fn sort_dac(arr: Vec<i32>) -> Vec<i32> {
     let arr_len = arr.len();
     let mut temp: Vec<i32> = Vec::with_capacity(arr_len);
     temp.resize(arr_len, 0);
     
-    sort_dac_recursive(arr, &mut temp, 0, arr_len);
+    let (arr, temp) = sort_dac_recursive(arr, temp, 0, arr_len);
+
+    return arr;
 }
 
-fn sort_dac_recursive(arr: &mut Vec<i32>, temp: &mut Vec<i32>, start: usize, end: usize) {
+fn sort_dac_recursive(arr: Vec<i32>, temp: Vec<i32>, start: usize, end: usize) -> (Vec<i32>, Vec<i32>) {
     if start + 1 == end {
-        return
+        return (arr, temp)
     }
     let middle = (start + end) / 2;
-    sort_dac_recursive(arr, temp, start, middle);
-    sort_dac_recursive(arr, temp, middle, end);
+    let (arr, temp) = sort_dac_recursive(arr, temp, start, middle);
+    let (mut arr, mut temp) = sort_dac_recursive(arr, temp, middle, end);
 
     {
         let mut i = start;
         let mut j = middle;
-        let mut k: usize = start;
+        let mut k = start;
         
         while i < middle && j < end {
             if arr[i] < arr[j] {
@@ -55,4 +57,5 @@ fn sort_dac_recursive(arr: &mut Vec<i32>, temp: &mut Vec<i32>, start: usize, end
     for i in start..end {
         arr[i] = temp[i];
     }
+    (arr, temp)
 }
